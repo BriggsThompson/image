0efef0f5c588c0183966560c4b278dcd920b646a
Briggs Thompson <w.briggs.thompson@gmail.com>
2020-10-02 20:31:25 -0500

    Adding cookie reader for private URLs

--- src/ui.js
+++ src/ui.js
@@ -141,6 +141,22 @@ export default class Ui {
     this.toggleStatus(Ui.status.EMPTY);
   }
 
+  cookie(name) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
   /**
    * Shows an image
    *
@@ -153,8 +169,13 @@ export default class Ui {
      */
     const tag = /\.mp4$/.test(url) ? 'VIDEO' : 'IMG';
 
+    let queryString = '';
+    if (this.config.readCookie){
+      queryString = this.cookie(readCookie) || '';
+    }
+
     const attributes = {
-      src: url,
+      src: url + queryString,
     };
 
     /**
